68463b9e1b2db2878bdbabcbc8fb7db163c55b7f
Chrstphr <somto565@gmail.com>
2023-07-16 21:33:09 +0200

    set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 
 function App() {
   return (
-    <div className='App'>
-      app
+    <div>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <button class="btn">Button</button>
     </div>
   )
 }
