44d6c8f57e56b0795b55d9a498101a2cc5b7d01d
Chrstphr <somto565@gmail.com>
2023-07-16 23:49:19 +0200

    message function

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -1,8 +1,22 @@
   
 const ChatBox = () => {
+
+    const messages = [
+        {
+            id: 1,
+            message: 'well'
+        },
+        {
+            id: 2,
+            message: 'well again'
+        }
+    ]
+
   return (
-    <div>
-        ChatBox
+    <div className="pb-44 pt-28 containerWrap">
+        {messages.map(message => {
+            
+        })}
     </div>
   )
 }
