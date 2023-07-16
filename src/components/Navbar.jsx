77ee17391d5360c70d7a1ed5692ec417909391d1
Chrstphr <somto565@gmail.com>
2023-07-16 21:56:12 +0200

    logout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,12 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <>
-      <div className="containerWrap navbar bg-neutral text-neutral-content">
-        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+    <div className="navbar bg-neutral text-neutral-content ">
+      <div className="containerWrap flex justify-between">
+        <a className="btn btn-ghost normal-case text-xl">InstantChat</a>
+        <button>Logout</button>
       </div>
-    </>
+    </div>
   );
 };
 
