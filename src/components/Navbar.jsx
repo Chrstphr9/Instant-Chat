4e4c4184a65e96c2214bdd51a3ca442602ebbceb
Chrstphr <somto565@gmail.com>
2023-07-17 21:37:39 +0200

    finished logout function

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import { UserAuth } from "../context/AuthContext";
 
 const Navbar = () => {
+  const { currentUser, logout } = UserAuth();
+
+  const handleLogout = async () => {
+    try {
+      await logout();
+    } catch(error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div className="navbar bg-neutral text-neutral-content ">
       <div className="containerWrap flex justify-between">
         <a className="btn btn-ghost normal-case text-xl">InstantChat</a>
-        <button className="btn btn-outline">Logout</button>
+        {/* <button onClick={handleLogout} className="btn btn-outline">Logout</button> */}
+        {currentUser ? <button onClick={handleLogout}>Logout</button> : ''}
       </div>
     </div>
   );
