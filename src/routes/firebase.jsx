f2ddf1729d31376b58397bb7b21b0c8fa99a7aeb
Chrstphr <somto565@gmail.com>
2023-07-17 11:11:40 +0200

    imported auth

--- src/routes/firebase.jsx
+++ src/routes/firebase.jsx
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBu37iC4Fq9hCCyd5nzTrNrzR_mL-yDCYM",
   authDomain: "instantchat-94369.firebaseapp.com",
@@ -15,3 +12,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
