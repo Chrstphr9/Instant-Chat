047245f5cbd7b285e334a8c3ca3b74813edd79c0
Chrstphr <somto565@gmail.com>
2023-07-17 23:19:59 +0200

    imported firestore

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBu37iC4Fq9hCCyd5nzTrNrzR_mL-yDCYM",
@@ -12,4 +13,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app)
+export const auth = getAuth(app);
+const db = getFirestore(app);
