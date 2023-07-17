2b7edf8adf7223d062f34c18ef429e604e1e39ab
Chrstphr <somto565@gmail.com>
2023-07-17 21:21:07 +0200

    logout function

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useState, useContext, useEffect }  from "react";
-import { GoogleAuthProvider, onAuthStateChanged, signInWithRedirect } from "firebase/auth";
+import { GoogleAuthProvider, onAuthStateChanged, signInWithRedirect, signOut } from "firebase/auth";
 import { auth } from "../firebase";
 
 // create context
@@ -9,15 +9,20 @@ const AuthContext = createContext();
 export const AuthProvider = ({children}) => {
     const [currentUser, setCurrentUser] = useState('null');
 
+// signin with google
 const signinWithGoogle = ()=> {
     const provider = new GoogleAuthProvider(); 
     signInWithRedirect(auth, provider)
 }
+// sign out
+const logout = () => signOut(auth)
+
 
     const value = {
         currentUser,
         setCurrentUser,
-        signinWithGoogle
+        signinWithGoogle,
+        logout
     }
 
     //set currentUser 
