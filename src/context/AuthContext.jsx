aa037c4b4a1f86401e0737e07e97e3fe625c3228
Chrstphr <somto565@gmail.com>
2023-07-17 20:23:02 +0200

    added redirect fucntion

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,11 +1,19 @@
 import { createContext, useState, useContext }  from "react";
+import { GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
 
 // create context
 const AuthContext = createContext();
 
 // Provider Context
 export const AuthProvider = ({children}) => {
-    const [currentUser, setCurrentUser] = useState('john');
+    const [currentUser, setCurrentUser] = useState('null');
+
+const signinWithgoogle = ()=> {
+    const provider = new GoogleAuthProvider(); 
+    signInWithRedirect(auth, )
+    
+    
+}
 
     const value = {
         currentUser,
