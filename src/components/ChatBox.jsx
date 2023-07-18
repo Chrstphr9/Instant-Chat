f130d482d0c7e8effeb6338a9f41a4d773a1b430
Chrstphr <somto565@gmail.com>
2023-07-18 10:46:30 +0200

    linked databse info

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -1,28 +1,33 @@
-import Message from "./Message"
+import { useEffect, useState } from "react";
+import Message from "./Message";
+import { collection, query, where, onSnapshot } from "firebase/firestore";
+import { db } from "../firebase";
 
-  
 const ChatBox = () => {
+    const [messages, setMassages] = useState([]);  
 
-    const messages = [
-        {
-            id: 1,
-            text: 'well',
-            name: 'Chrstphr'
-        },
-        {
-            id: 2,
-            text: 'well again',
-            name: "Octane"
-        }
-    ]
+  useEffect(() => {
+    const q = query(
+        collection(db, "messgaes")
+        );
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const messages = [];
+      querySnapshot.forEach((doc) => {
+        messages.push({ ...doc.data(), id: doc.id });
+      });
+      setMassages(messages);
+    });
+
+    return unsubscribe;
+  }, []);
 
   return (
     <div className="pb-44 pt-28 containerWrap">
-        {messages.map(message => (
-            <Message key={message.id } message={message} /> 
-        ))}
+      {messages.map((message) => (
+        <Message key={message.id} message={message} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default ChatBox
+export default ChatBox;
