7ce0888c8a9c9372e5a2d15b3dd8e5eecb47b494
Chrstphr <somto565@gmail.com>
2023-07-18 14:20:06 +0200

    done

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -1,10 +1,17 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Message from "./Message";
 import { collection, query, where, onSnapshot, orderBy, limit } from "firebase/firestore";
 import { db } from "../firebase";
 
 const ChatBox = () => {
+    const messagesEndRef = useRef();
     const [messages, setMessages] = useState([]);  
+
+    const scrollToBottom = () => {
+        messagesEndRef.current.scrollIntoView({ behavior: 'smooth'})
+    };
+
+    useEffect(scrollToBottom, [messages])
 
   useEffect(() => {
     
@@ -29,6 +36,9 @@ const ChatBox = () => {
       {messages.map((message) => (
         <Message key={message.id} message={message} />
       ))}
+      <div ref={messagesEndRef}>
+
+      </div>
     </div>
   );
 };
